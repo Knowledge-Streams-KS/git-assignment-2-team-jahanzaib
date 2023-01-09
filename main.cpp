5afa60a71ad7ba4b03c55f14f3e2709b27ee287c
Jahanzaib malik <jahanzaibmalik.321@gmail.com>
2023-01-09 22:31:24 +0500

    updated develop branch and added subtraction header file

--- main.cpp
+++ main.cpp
@@ -4,12 +4,8 @@ using namespace std;
 
 void main()
 {
-    cout<<"Creating a calculator"<<endl;
+    cout<<"Creating a calculator"<<endl<<endl;
     int a,b,opt,ans;
-    cout<<"Enter First number";
-    cin >> a;
-    cout<<"Enter Second number";
-    cin >> b;
 
     cout<<"Select Option: "<<endl;
     cout<<"1. Addition"<<endl;
@@ -20,26 +16,51 @@ void main()
     cout<<"6. Square Root"<<endl;
     cout<<"Enter your option : ";
     cin>>opt;
+    cout<<endl<<endl;
 
     if(opt==1)//Addition
     {
+        cout<<"Enter First number : ";
+        cin >> a;
+        cout<<"Enter Second number : ";
+        cin >> b;
 
     }
     else if(opt==2)//Subtraction
     {
-        
+        cout<<"Enter First number : ";
+        cin >> a;
+        cout<<"Enter Second number : ";
+        cin >> b;
+        cout<<a<<" - "<<b<<" = "<<subtraction(a,b)<<endl;
     }
     else if(opt==3)//Multiplication
     {
+        cout<<"Enter First number : ";
+        cin >> a;
+        cout<<"Enter Second number : ";
+        cin >> b;
         
     }
     else if(opt==4)//Divion
-    {
+    {    
+        cout<<"Enter First number : ";
+        cin >> a;
+        cout<<"Enter Second number : ";
+        cin >> b;
         
     }
     else if(opt==5)//Modulus
     {
+        cout<<"Enter First number : ";
+        cin >> a;
+        cout<<"Enter Second number : ";
+        cin >> b;
         
     }
     else if(opt==6)//Square Root
+    {
+        cout<<"Enter the Number for Square Root : ";
+        cin>>a;
+    }
 }
