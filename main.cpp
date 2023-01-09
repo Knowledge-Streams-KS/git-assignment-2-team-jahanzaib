08ec4acb26110db190b25821d70db6340b959c2f
Jahanzaib malik <jahanzaibmalik.321@gmail.com>
2023-01-09 14:54:53 +0500

    implemented main

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+void main()
+{
+    cout<<"Creating a calculator"<<endl;
+    int a,b,opt;
+    cout << "Enter Furst number";
+    cin >> a;
+    cout << "Enter Furst number";
+    cin >> b;
+
+    cout << "Select Option: "<<endl;
+    cout<<"1. Addition"<<endl;
+    cout<<"2. Subtraction"<<endl;
+    cout<<"3. Multiplication"<<endl;
+    cout<<"4. Division"<<endl;
+    cout<<"5. Modulus"<<endl;
+    cout<<"6. Square Root"<<endl;
+    cout<<"Enter your option : ";
+    cin>>opt;
+
+    if(opt==1)
+    {
+
+    }
+    else if(opt==2)
+    {
+        
+    }
+    else if(opt==3)
+    {
+        
+    }
+    else if(opt==4)
+    {
+        
+    }
+    else if(opt==5)
+    {
+        
+    }
+    else if(opt==6)
+}
